e5f44485cc46617b801b4bfe04ff0d4698ad17a1
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-29 13:42:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import streamlit as st
 
 openai.api_key = "YOUR_OPENAI_API_KEY_HERE"
 
-def classify_sentiment(prompt, engine):
+def classify_sentiment(prompt, engine,temperature, max_tokens, top_p, frequency_penalty, presence_penalty):
     response = openai.Completion.create(
         engine=engine,
         prompt=prompt,
-        max_tokens=1,
+        max_tokens=max_tokens,
         n=1,
         stop=None,
-        temperature=0.5
+        temperature=temperature,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty
     )
 
     sentiment = response.choices[0].text.strip()
@@ -58,6 +60,23 @@ def main():
             prompt = f"Sentiment Analysis: {user_input}\nSentiment:"
             sentiment = classify_sentiment(prompt, engine_id)
             st.write(f"Sentiment: {sentiment}")
+            
+        # Display chat settings sidebar
+        st.sidebar.title("Paramètres")
+        settings["engine"] = st.sidebar.selectbox("Engine", list(engine_options.keys()))
+        settings["temperature"] = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, step=0.1, value=settings["temperature"])
+        settings["max_tokens"] = st.sidebar.slider("Max Tokens", min_value=1, max_value=2048, step=1, value=settings["max_tokens"])
+        settings["top_p"] = st.sidebar.slider("Top P", min_value=0.0, max_value=1.0, step=0.1, value=settings["top_p"])
+        settings["frequency_penalty"] = st.sidebar.slider("Frequency Penalty", min_value=0.0, max_value=1.0, step=0.1, value=settings["frequency_penalty"])
+        settings["presence_penalty"] = st.sidebar.slider("Presence Penalty", min_value=0.0, max_value=1.0, step=0.1, value=settings["presence_penalty"])
+                                                              # Display current settings
+        st.sidebar.markdown("### Paramètres Actuels")
+        st.sidebar.write(f"Engine: {settings['engine']}")
+        st.sidebar.write(f"Temperature: {settings['temperature']}")
+        st.sidebar.write(f"Max Tokens: {settings['max_tokens']}")
+        st.sidebar.write(f"Top P: {settings['top_p']}")
+        st.sidebar.write(f"Frequency Penalty: {settings['frequency_penalty']}")
+        st.sidebar.write(f"Presence Penalty: {settings['presence_penalty']}")         
 
 if __name__ == "__main__":
     main()
